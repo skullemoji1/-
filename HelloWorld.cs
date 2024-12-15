cf45edde06a3413abba64f0c1359d3f08c8d592d
skullemoji1 <98308778+skullemoji1@users.noreply.github.com>
2024-12-15 12:14:21 +0100

    HelloWorld

---
+++ HelloWorld.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+
+    void Start()
+    {
+        //seks
+        print("hello world");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
